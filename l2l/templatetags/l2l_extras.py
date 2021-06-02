4b14d38ca98d76964c66e2b16a659f3e60047e18
ThoshHub <shamthosh@gmail.com>
2021-06-02 05:08:37 -0400

    first commit

---
+++ l2l/templatetags/l2l_extras.py
@@ -0,0 +1,19 @@
+from django import template
+import datetime
+
+register = template.Library()
+
+def l2l_dt(date_val):
+
+    if(isinstance(date_val, str)): # if string, convert it into date object and return proper format
+        date_time_obj = datetime.datetime.strptime(date_val, "%Y-%m-%dT%H:%M:%S")
+        return date_time_obj.strftime('%Y-%m-%d %H:%M:%S')
+    else: # otherwise, return proper format
+        print("datetime")   
+        return date_val.strftime('%Y-%m-%d %H:%M:%S')
+    
+    # print(str(date_val) + ", " + str(type(date_val)))
+    
+    return date_val
+
+register.filter('l2l_dt', l2l_dt)
